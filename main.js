fd4648ae6d23bd6f00e3dfea260bc451780bf4f0
GalaxyWolfCloud <79249476+GalaxyWolfCloud@users.noreply.github.com>
2021-06-11 14:18:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function add_item()
     htmldata="<section class='cards'>"
     for(var i=0;i<menu_list_array.length;i++)
     {
-        htmldata=htmldata+'<div class="card">'+'<img src = "images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+        htmldata=htmldata+'<div class="card">'+'<img src = "https://github.com/GalaxyWolfCloud/Pizzeria-Project79/blob/main/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
         
     }
       htmldata=htmldata+"</section>"
